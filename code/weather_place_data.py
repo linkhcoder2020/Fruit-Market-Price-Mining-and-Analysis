b067d9d4b860dd8509f9a3c61feb298fe5a52e90
linkh <joylin00967@gmail.com>
2020-08-10 21:24:37 -0700

    update

--- code/weather_place_data.py
+++ code/weather_place_data.py
@@ -12,9 +12,8 @@ from weather_place_api import grab_data_from_place_api,grab_data_from_weather_ap
 
 def get_weather_place_data(path):
     city = ["Atlanta","Baltimore","Boston","Chicago","Columbia","Detroit","Los Angeles","MiaMi","New York","Philadelphia"]
-    place_key = "AIzaSyDMi_xHrr4kvuqVkEPPVPJAyG5lM7cXmyQ"
-    # weather_key = "ceb5e81c09ea372fad329d7c6924f1b8" 
-    weather_key = "3ed65b3120c5b1b0afd57ee511a79c8e"
+    place_key = "(put your API key here)"
+    weather_key = "(put your API key here)"
 
     print('Grab data from place API')
     place_df = grab_data_from_place_api(city,place_key,path)
